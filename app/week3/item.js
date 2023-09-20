78a781c910971fe234f3d68ebfb872a50c377974
JasonLMarshall <126294193+JasonLMarshall@users.noreply.github.com>
2023-09-20 15:42:10 -0600

    Adding more styling to Shopping List

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,6 +1,6 @@
 export default function Item({name, quantity, category}) {
     return (
-        <ul className="border border-sky-500 bg-blue-900 w-96 m-4 p-2">
+        <ul className="border border-sky-500 bg-blue-900 w-96 m-4 p-2 rounded-xl hover:` bg-green-600`" >
             <li><h3 className= "text-xl font-bold"> {name} </h3></li>
             <li>Buy {quantity} in {category}</li>            
         </ul>
